054330f085a945d2bdb18a5e08333554e066b218
Nixinerix <xoamara2@gmail.com>
2019-01-21 19:23:22 -0800

    Initial files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Beast Mode');
+INSERT INTO burgers (burger_name) VALUES ('Double Double');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Royale with Cheese');
